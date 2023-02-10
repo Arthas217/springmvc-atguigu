1d9edb6870b7897c361149604cd467611d02c063
zuocheng <442854399@qq.com>
2023-02-10 13:25:59 +0800

    Initial commit

---
+++ springmvc-01/src/main/java/com/atguigu/mvc/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author 会游泳的蚂蚁
+ * @Description:  注入控制器组件到ioc容器
+ * 1、注解@Controller+扫描方式component-scan
+ * 2、bean标签---xml方式
+ * @Date 2023/2/10 13:05
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        //设置视图名称
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String HelloWorld() {
+        return "target";
+    }
+}
