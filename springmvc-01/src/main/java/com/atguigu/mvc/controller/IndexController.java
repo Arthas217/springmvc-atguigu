1929fbf8fd09107d1f6648a0ea531a4a98bcefd7
zuocheng <442854399@qq.com>
2023-02-18 21:19:16 +0800

    add

---
+++ springmvc-01/src/main/java/com/atguigu/mvc/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.atguigu.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author 会游泳的蚂蚁
+ * @Description:
+ * @Date 2023/2/18 20:58
+ */
+@Controller
+public class IndexController {
+
+    /**
+     * 对首页的访问
+     *
+     * @return
+     */
+//    @RequestMapping("/")
+//    public String index() {
+//        //设置视图名称
+//        return "index";
+//    }
+
+}
