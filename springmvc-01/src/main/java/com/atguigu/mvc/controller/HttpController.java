3c319b551a6f7e630179da2d40b1bacc1a32f37e
zuocheng <442854399@qq.com>
2023-02-24 23:49:28 +0800

    add

--- springmvc-01/src/main/java/com/atguigu/mvc/controller/HttpController.java
+++ springmvc-01/src/main/java/com/atguigu/mvc/controller/HttpController.java
@@ -59,9 +59,16 @@ public class HttpController {
 
 
     /**
+     * json是JavaScript里的一个数据格式
+     * json类型：json对象{}---实体类、map  ，json数组[]-----list
+     * xml也是一种一个数据交互格式，一般用来做配置
+     */
+
+    /**
      * User对象是否可以作为方法的返回值直接响应浏览器数据
-     * 答案不行浏览器只能识别文本，可以用json这种数据交互的格式
-     * 解决方式 1、json依赖 2、配置开启mvc注解驱动 + 方法上加@ResponseBody + 方法返回值为对象
+     * 答案不行浏览器只能识别文本，可以用json数据交互的格式
+     * springmvc解决方式 1、json依赖 2、配置开启mvc注解驱动 + 方法上加@ResponseBody + 方法返回值为对象
+     * 那么java对象自动转为为json格式的字符串
      * @return
      * @throws IOException
      */
@@ -69,6 +76,19 @@ public class HttpController {
     @ResponseBody
     public User testResponseBody2() throws IOException {
         return new User(1,"vampire","123","男","23","1@qq.com");
+    }
+
+
+    /**
+     * springmvc处理ajax页面，不刷新与服务器交互，服务端不能通过转发、重定向，只能通过响应浏览器数据
+     * 注意需要mvn package
+     * ajax页面不跳转的情况下与服务器进行数据交换的例子
+     */
+    @RequestMapping("/testAxios")
+    @ResponseBody
+    public String testAxios(String username,String password){
+        System.out.println(username+" "+password);
+        return "hello,axios";
     }
 
 }
