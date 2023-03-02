8c717913444d5adcb39b55b4220b16fc8df9be1f
zuocheng <442854399@qq.com>
2023-03-02 12:09:55 +0800

    多个拦截器

--- springmvc-01/src/main/java/com/atguigu/mvc/interceptors/MultiRequestInterceptor.java
+++ springmvc-01/src/main/java/com/atguigu/mvc/interceptors/MultiRequestInterceptor.java
@@ -9,37 +9,27 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Author 会游泳的蚂蚁
- * @Description: 拦截器用于拦截控制器方法的执行；
- * 需要实现HandlerInterceptor；
- * springmvc拦截器必须在配置文件中进行配置
- * @Date 2023/3/1 22:25
+ * @Description:
+ * @Date 2023/3/1 23:36
  */
 @Component
-public class AllRequestInterceptor implements HandlerInterceptor {
-    /**
-     * 控制器方法执行之前执行，返回值标识是否拦截或方形，true放行 false拦截即不调用控制器方法
-     */
+public class MultiRequestInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("--------FirstInterceptor-------preHandle-----------");
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        System.out.println("--------MultiRequestInterceptor-------preHandle-----------");
+//        return HandlerInterceptor.super.preHandle(request, response, handler);
+        return false;//拦截状态
     }
 
-    /**
-     * 控制器方法执行之后执行
-     */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("--------FirstInterceptor-------postHandle-----------");
+        System.out.println("--------MultiRequestInterceptor-------postHandle-----------");
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
-    /**
-     * 处理完视图和模型数据，渲染视图完毕之后执行
-     */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println("--------FirstInterceptor-------afterCompletion-----------");
+        System.out.println("--------MultiRequestInterceptor-------afterCompletion-----------");
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 }
